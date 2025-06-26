41bf1c13b4431583b54408a26732617ba89e1039
SpacePirate27 <ramnarayanbahadur@gmail.com>
2025-06-27 00:59:43 +0530

    feat: Update rules to new format

--- src/lexiconis/core.clj
+++ src/lexiconis/core.clj
@@ -3,33 +3,33 @@
             [lexiconis.specs :as spec]))
 
 (defrule sleep-time {::spec/rule-id "Sleep"
-                     ::spec/if      {::spec/room :bedroom
-                                     ::spec/time 1750957200000}
+                     ::spec/if      {::spec/room [:eq :bedroom]
+                                     ::spec/time [:gt 1750957200000]}
                      ::spec/then    [{:ac :on}
                                      {:tv :off}
                                      {:lights :dim}]})
 
 (defrule morning-coffee {::spec/rule-id "Morning Coffee"
-                         ::spec/if      {::spec/room :kitchen
-                                         :spec/time  1750897800000}
+                         ::spec/if      {::spec/room [:eq :kitchen]
+                                         :spec/time  [:gt 1750897800000]}
                          ::spec/then    [{:coffee-machine :on}]})
 
 (defrule blinding-lights {::spec/rule-id "Blinding Lights"
-                          ::spec/if {::spec/room :hall
-                                     ::spec/time 1750919400000
-                                     ::spec/lux-level 10000}
-                          ::spec/then [{:blinds :off}]})
+                          ::spec/if      {::spec/room      [:eq :hall]
+                                          ::spec/time      [:gt 1750919400000]
+                                          ::spec/lux-level [:gt 10000]}
+                          ::spec/then    [{:blinds :off}]})
 
 (defrule no-tv {::spec/rule-id "No TV"
-                ::spec/if {::spec/room :hall
-                           ::spec/motion false}
-                ::spec/then [{:tv :off}]})
+                ::spec/if      {::spec/room   [:eq :hall]
+                                ::spec/motion false}
+                ::spec/then    [{:tv :off}]})
 
 (defrule holy-smokes {::spec/rule-id "Holy Smokes"
-                      ::spec/if {::spec/room :kitchen
-                                 ::spec/smoke true}
-                      ::spec/then [{:gas-valve :off}
-                                   {:alarm :on}
-                                   {:notification :send}]})
+                      ::spec/if      {::spec/room  [:eq :kitchen]
+                                      ::spec/smoke true}
+                      ::spec/then    [{:gas-valve :off}
+                                      {:alarm :on}
+                                      {:notification :send}]})
 
 
