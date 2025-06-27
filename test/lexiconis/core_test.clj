8b43d4e42d91127e375d05df21242a4f229785db
SpacePirate27 <ramnarayanbahadur@gmail.com>
2025-06-27 11:13:03 +0530

    feat: Add test for sleep-time rule

--- test/lexiconis/core_test.clj
+++ test/lexiconis/core_test.clj
@@ -1,7 +1,18 @@
 (ns lexiconis.core-test
   (:require [clojure.test :refer :all]
-            [lexiconis.core :refer :all]))
+            [lexiconis.specs :as spec]
+            [lexiconis.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sleep-time-test
+  (testing "Given a valid sleep-time fact, actions are performed"
+    (is (= [{:action :ac :status true}
+            {:action :tv :status true}
+            {:action :lights :status true}]
+           (core/sleep-time {::spec/room-type :bedroom
+                             ::spec/time-input 1751034400000}))))
+  (testing "Given a invalid sleep-time fact, rule should fail"
+    (is (= {:rule :failed :status false} (core/sleep-time {::spec/room-type :kitchen
+                                                           ::spec/time-input 1751034400000}))))
+  (testing "Given an invalid input fact, input validation should fail"
+    (is (= {:input-validation :failed :status false} (core/sleep-time {::spec/a :b})))))
+
